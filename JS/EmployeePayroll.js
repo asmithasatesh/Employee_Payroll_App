6384ad1d58acce3dce34a6fc99f08ebc5ef36e99
asmithasatesh <asmithasatesh@gmail.com>
2021-08-25 14:37:59 +0530

    [Asmitha]Added: Create employee ID and save data on Local Storage

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -3,6 +3,14 @@
 
 class EmployeePayrollData
 {
+    get id()
+    {
+        return this._id;
+    }
+    set id(id)
+    {
+        this._id=id;
+    }
     get name()
     {
         return this._name;
